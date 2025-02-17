95447b66af2ab8ac30808ff88d1bf39acb8b86f5
SSAYKO <sandresmurillo@outlook.com>
2025-02-17 15:47:37 -0500

    modified javascript file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-        alert('Button was clicked!');
-    });
+document.addEventListener("DOMContentLoaded", (event) => {
+  const button = document.getElementById("myButton");
+  button.addEventListener("click", () => {
+    alert("Button was sooooo clickedddd!");
+  });
 });
