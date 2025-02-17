a29fe3d09eede9a08a5dc4cea9b9da24011fcedc
SSAYKO <sandresmurillo@outlook.com>
2025-02-17 17:01:35 -0500

    added and updated side bar

--- index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-        alert('Button was clicked!');
-    });
-});
